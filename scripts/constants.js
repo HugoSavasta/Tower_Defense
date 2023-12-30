8b4266e66e4dc7ecde9dd48b4409c0f62d0c8e75
@hsavasta <hugo.savasta@etu.univ-cotedazur.fr>
2023-12-30 13:28:56 +0100

    add file constant

---
+++ scripts/constants.js
@@ -0,0 +1,5 @@
+export const cellSize = 100;
+export const cellGap = 3;
+export const frames_per_second = 60;
+export const frame_interval = 1000 / frames_per_second;
+export const floatingMessages = [];
